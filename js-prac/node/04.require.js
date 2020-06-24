4d24a0a86797350e51722240a3ea2a1ea0dbff73
vander <vanderdeng@gmail.com>
2020-06-24 22:28:22 +0800

    full stack update

---
+++ js-prac/node/04.require.js
@@ -0,0 +1,2 @@
+const a = require('./helloworld');
+console.log(a.greeting('zhangshan'));
---
+++ js-prac/node/helloworld.js
@@ -0,0 +1,6 @@
+var first = "hello nodejs";
+console.log(first);
+
+const greeting = name => `hello ${name}`;
+module.exports.greeting = greeting;
+
