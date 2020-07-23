501dbfa517165822d25d6f881ce042bb22df87eb
vander <vanderdeng@gmail.com>
2020-07-24 07:56:13 +0800

    update

---
+++ js-prac/react/react-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Square from './Square';
+
+function App() {
+  return (
+    <div className="App">
+      <Square></Square>
+    </div>
+  );
+}
+
+export default App;
---
+++ js-prac/react/react-demo/src/Square.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Square = () => {
+    return <button>X</button>;
+};
+
+export default Square;
